14b2930b801c5a62d83b605ae0c38e054a7c3a65
haruki-mochida <motimotimotida@gmail.com>
2023-08-14 23:27:27 +0900

    次の画面に遷移するように修正

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,12 +1,18 @@
 window.onload = function() {
-    setInterval(function(){
+    // intervalIDを変数で保持する
+    const intervalID = setInterval(function(){
         fetch('/progress')
         .then(response => response.text())
         .then(data => {
             document.getElementById('progress').innerText = 'Progress: ' + data + '%';
             if (parseInt(data) === 100) {
+                // 進捗が100%に達した場合、ポーリングを停止する
+                clearInterval(intervalID);
                 window.location.href = '/analysis';
             }
+        })
+        .catch(error => {
+            console.error('Error fetching progress:', error);
         });
-    }, 1000);  // 1000ミリ秒（1秒）ごとにリクエストを送る
+    }, 1000);
 };
